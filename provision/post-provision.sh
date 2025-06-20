94ed3fb499a3161382052cb6f2f33a2a55c58c24
Jarbas Peixoto Junior <jarbas.junior@gmail.com>
2025-06-20 12:52:02 -0400

    Implementar um pos provision

--- provision/post-provision.sh
+++ provision/post-provision.sh
@@ -5,7 +5,7 @@ samba-tool user setexpiry Administrator --noexpiry
 samba-tool domain passwordsettings set --complexity=on --history-length=3 --min-pwd-age=0 --max-pwd-age=365 --min-pwd-length=8
 
 # Validando politicas do Domínio
-samba-tool domain passwordsettings show
+# samba-tool domain passwordsettings show
 
 # Validar a versão do esquema do AD em um Samba DC
-samba-tool domain level show
+# samba-tool domain level show
